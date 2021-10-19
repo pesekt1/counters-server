457ae5668589b7132602620d04f09718cfe98ccb
Tomas Pesek <pesekt@gmail.com>
2021-10-19 17:37:36 +0200

    refactor

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const config = require("config");
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -9,11 +8,8 @@ app.get("/", (req, res) => {
 //listening on port:
 const port = process.env.PORT || 3001;
 
-//host:
-const host = config.get("host");
-
-const server = app.listen(port, () => {
+app.listen(port, () => {
   console.log(`Example app listening at ${port}`);
 });
 
-module.exports = server;
+//module.exports = server;
