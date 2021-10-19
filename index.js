755104ded6d3994ad6c50e8964e36a112d9f537a
Tomas Pesek <pesekt@gmail.com>
2021-10-19 17:59:36 +0200

    api with fake data

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+//API with fake data - mocking the database
+app.get("/api/counters", (req, res) => {
+  res.send([
+    { id: 1, value: 0, liked: false },
+    { id: 2, value: 0, liked: false },
+    { id: 3, value: 0, liked: false },
+  ]);
+});
+//
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
