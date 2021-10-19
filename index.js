8f30332ef7089a8007a9d347a2fb1fc5cf6c2502
Tomas Pesek <pesekt@gmail.com>
2021-10-19 22:04:06 +0200

    cors

--- index.js
+++ index.js
@@ -1,19 +1,68 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+//cross origin allowed
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+let counters = [
+  { id: 1, value: 0, liked: false },
+  { id: 2, value: 0, liked: false },
+  { id: 3, value: 0, liked: false },
+  { id: 4, value: 0, liked: false },
+];
+
 //API with fake data - mocking the database
 app.get("/api/counters", (req, res) => {
-  res.send([
-    { id: 1, value: 0, liked: false },
-    { id: 2, value: 0, liked: false },
-    { id: 3, value: 0, liked: false },
-  ]);
+  res.header("Access-Control-Allow-Origin", "*");
+  res.send(counters);
 });
-//
+
+app.delete("/api/counters", (req, res) => {
+  counters = [];
+  res.status(200).send("all deleted");
+});
+
+app.delete("/api/counters/:id", (req, res) => {
+  let deletedCounter = counters.find((c) => c.id === req.params.id);
+  const index = counters.indexOf(deletedCounter);
+  counters.splice(index, 1);
+  res.send(deletedCounter);
+});
+
+app.post("/api/counters", (req, res) => {
+  const newId = counters.length ? counters[counters.length - 1].id + 1 : 1;
+  const newCounter = { id: newId, value: 0, liked: false };
+  counters.push(newCounter);
+  res.send(newCounter);
+});
+
+app.put("/api/counters/:id", (req, res) => {
+  const counter = req.body;
+  const counterInDb = counters.find((c) => c.id === counter.id);
+  const index = counters.indexOf(counterInDb);
+  counters.splice(index, 1, counter);
+  res.send(counter);
+});
+
+app.put("/api/counters/", (req, res) => {
+  counters.map((c) => {
+    c.value = 0;
+  });
+  res.send(counters);
+});
+
 const port = process.env.PORT || 3001;
 
 app.listen(port, () => {
